cc826627b839ac32202039ebd05c4c4152a48a98
keruto <krizyzone@hotmail.com>
2016-09-24 19:41:20 +0200

    360 demo

---
+++ TestProject_VR Interaction/Assets/HideHighTiles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideHighTiles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Tile")
+        {
+            other.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Tile")
+        {
+            other.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}

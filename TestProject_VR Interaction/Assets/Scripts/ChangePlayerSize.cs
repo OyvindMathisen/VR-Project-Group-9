2b5f5b43e12c3dcd831c2637c7e20ba6065cc95b
PerfectRaito <PerfectRaito@hotmail.com>
2016-10-03 19:25:03 +0200

    Scaled up models to allow downscaling to walk around in the town. Script to switch between edit mode and exploration. Teleportation while in exploration mode.

---
+++ TestProject_VR Interaction/Assets/Scripts/ChangePlayerSize.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePlayerSize : MonoBehaviour {
+
+	public GameObject CameraRig;
+	public GameObject panel;
+
+	private Wand Lhand;
+	private bool isShrunk;
+
+	void Awake ()
+	{
+		Lhand = GameObject.Find("[CameraRig]").transform.FindChild("Controller (left)").GetComponent<Wand>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Lhand.gripButtonDown)
+		{
+			if (isShrunk)
+			{
+				CameraRig.transform.localScale = new Vector3(100.0f, 100.0f, 100.0f);
+				CameraRig.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+
+				panel.SetActive(true);
+				GetComponent<SteamVR_LaserPointer>().active = false;
+				GetComponent<SteamVR_Teleporter>().teleportOnClick = false;
+				isShrunk = false;
+			}
+			else
+			{
+				CameraRig.transform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+				CameraRig.transform.position = new Vector3(0.0f, 101.0f, 5.5f);
+
+				panel.SetActive(false);
+				GetComponent<SteamVR_LaserPointer>().active = true;
+				GetComponent<SteamVR_Teleporter>().teleportOnClick = true;
+				isShrunk = true;
+			}
+		}
+	}
+}
--- TestProject_VR Interaction/Assets/Scripts/DragAndPlace.cs
+++ TestProject_VR Interaction/Assets/Scripts/DragAndPlace.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 public class DragAndPlace : MonoBehaviour
 {
-	private float BUILD_HEIGHT = 1.0f, BUILD_HEIGHT_LERP = 0.15f; // make the tiles stay at a certain height
+	private float BUILD_HEIGHT = 100.0f, BUILD_HEIGHT_LERP = 15.0f; // make the tiles stay at a certain height
 	private bool placed = false; // if the tile is still "dragged" around (the mouse button is not released yet)
-	private float SNAP_VALUE = 0.08f; // important for choosing grid size (do not edit unless you edit the tile sizes)
+	private float SNAP_VALUE = 8.0f; // important for choosing grid size (do not edit unless you edit the tile sizes)
 	float snapInverse;
 	private Wand Rhand;
 	private bool hasRotated = false;
